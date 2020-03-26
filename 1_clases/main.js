545227ba016d33fec517dbc4117b6ba256ebb6dc
maui <maui@devf.mx>
2020-03-25 20:43:35 -0600

    Agregar solución de ejercicio 2 (cuenta)

--- 1_clases/main.js
+++ 1_clases/main.js
@@ -60,3 +60,61 @@ const gato = new Mascota(
   "Persa",
   4,
 );
+
+
+
+/*
+  1.- Haz una clase llamada Persona que siga las siguientes condiciones:
+    Sus atributos son: nombre, edad, RFC, sexo, peso y altura.
+    calcularIMC()
+    esMayorDeEdad()
+    El constructor pide nombre, edad,sexo,peso y altura
+    Generar el RFC a partir de el nombre, edad y sexo
+*/
+/*
+  2.- Crear una clase Cuenta que tenga los siguientes atributos y metodos:
+    -Titular y cantidad
+    -ingresar(cantidad)
+    -retirar(cantidad)
+    Hacer las validaciones previas
+    Como regla de negocio no debe de tener más de $900 y menos de $10
+*/
+
+class Cuenta {
+  constructor(titular, cantidad) {
+    this.titular = titular;
+    this.cantidad = cantidad;
+  }
+
+  getTitular() {
+    return this.titular;
+  }
+
+  setTitular(titular) {
+    return this.titular = titular;
+  }
+
+  getCantidad() {
+    return this.cantidad;
+  }
+
+  // no debe de tener más de $900
+  ingresar(dinero) {
+    const cantidadFinal = this.cantidad + dinero;
+    if (cantidadFinal > 900) {
+      throw new Error("No puedes tener más de $900 en la cuenta");
+    }
+    return this.cantidad = cantidadFinal;
+  }
+
+  // no debe de tener menos de $10
+  retirar(dinero) {
+    const cantidadFinal = this.cantidad - dinero;
+    if (cantidadFinal < 10) {
+      throw new Error("No puedes tneer menos de $10 en la cuenta");
+    }
+    return this.cantidad = cantidadFinal;
+  }
+}
+
+const cuenta = new Cuenta("Maui", 27.50);
