82630264f7be4cbfae61125a5b6778767b76a0f4
maui <maui@devf.mx>
2020-03-27 20:36:23 -0600

    Agregar soluciones a ejercicios 1 y 2 de Async

--- 3_async/main.js
+++ 3_async/main.js
@@ -1,7 +1,11 @@
 const mostrarMensaje = () => console.log("Pasó un segundo!");
 
 // console.log("¡Funciona!");       // SINCRONO
-// setTimeout(mostrarMensaje, 0);   // ASYNC
+// FUNCION DE ORDEN SUPERIOR
+// setTimeout(                         // ASYNC
+//   mostrarMensaje,                // CALLBACK
+//   0
+// );      
 // console.log(3);                  // SINCRONO
 // console.log(4);                  // SINCRONO
 
@@ -33,3 +37,55 @@ const esParLaSumaDeDosNumeros = (numero1, numero2) => {
 // if (esPar2 && esPar3) console.log("Los dos numeros son pares!")
 
 // console.log(esParLaSumaDeDosNumeros(5, 7));
+// Element.addEventListener("click", callback)
+
+/*
+1.- Muestra un mensaje en consola mediante un callback. 
+  La función de orden superior que escribas debe poder mostrar el mensaje como console.warn, console.log, console.info, o cualquier método para pintar en consola del objeto console.
+*/
+
+// const mensajeEnConsola = (callback, mensaje) => {
+//   callback(mensaje);
+// }
+
+// mensajeEnConsola(console.info, "Hola");
+
+/*
+2.- Crear una función de orden superior que reciba como
+    argumento una variable de cualquier tipo y un callback. La función de orden superior debe retornar como resultado, mediante el callback, cual es el tipo de dato de la variable ingresada e imprimir su contenido.
+*/
+
+const higherOrder = (variable, callback) => {
+  callback(variable);
+}
+
+const obtenerTipoDeDato = (variable) => typeof(variable);
+
+const callbackContenido = (variable) => {
+  console.log(obtenerTipoDeDato(variable));
+  console.log(variable);
+}
+
+const miVariable = { nombre: "Maui" };
+higherOrder(miVariable, callbackContenido);
+
+/*
+3.- Crear una función de orden superior que reciba como 
+    argumentos dos números y un callback. Según el callback
+    que se pase a la función, se devuelve la suma de los
+    dos números, la resta de los dos números o la
+    multiplicación de los dos números.
+
+4.- Escribe una función de orden superior que reciba una cadena de
+    caracteres y debe devolver, mediante un callback, la
+    cadena de caracteres en mayúsculas o en minúsculas.
+            ordenSuperior("PejeLagarto", minus);
+            -> pejelagarto
+            ordenSuperior("PejeLagarto", mayus);
+            -> PEJELARTO
+
+5.- Hacer un arreglo de 4 cantidades de tiempo (en minutos)
+    EJEMPLO [120, 80, 200, 100] y tomar solo las cantidades
+    mayores a dos horas (hacer la comparación en horas) 
+    mediante un callback.
+*/
