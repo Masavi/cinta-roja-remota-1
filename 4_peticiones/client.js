72bc9cd6788abc81f838801f8383aa5163d14f2a
maui <maui@devf.mx>
2020-03-30 19:52:58 -0600

    agregar ejemplo de peticion get con request

---
+++ 4_peticiones/client.js
@@ -0,0 +1,12 @@
+const request = require('request');
+
+// console.log("Vamos a hacer una peticion!");
+// request.get('http://www.google.com', (error, response, body) => {
+//   // console.error('error:', error); // Print the error if one occurred
+//   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   // console.log('body:', body); // Print the HTML for the Google homepage.
+//   console.log('🚀 Cuerpo de la respuesta', body);
+//   console.log('🚀 Código de estado', response.statusCode);
+// });
+// console.log("Ha terminado la peticion...");
+
