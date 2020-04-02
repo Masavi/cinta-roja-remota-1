b3f119edb551a6c2c9a48e591f58e0eae8355b92
maui <maui@devf.mx>
2020-04-01 20:55:54 -0600

    Agregar CRUD authors

--- 5_crud/client.js
+++ 5_crud/client.js
@@ -22,8 +22,57 @@ const createAuthor = (name, last_name, nacionalidad, biography, gender, age) => 
   });
 }
 
-createAuthor("Gabriel", "Garcia", "MX", "Escritor de realismo mágico", "M", 90);
+// createAuthor("Gabriel", "Garcia", "MX", "Escritor de realismo mágico", "M", 90);
 
-//  Read   - GET
-//  Update - PUT/PATCH
-//  Delete - DELETE
+//  Read ALL   - GET
+const readAuthors = () => {
+  const URL = `${URL_BASE}/api/v1/authors/`
+  request.get(URL, (err, res, body) => {
+    console.log(res.statusCode);
+    console.log(JSON.parse(body));
+  });
+}
+
+// readAuthors();
+
+//  Read ONE   - GET
+const readOneAuthor = (id) => {
+  const URL = `${URL_BASE}/api/v1/authors/${id}/`;
+  request.get(URL, (err, res, body) => {
+    console.log(res.statusCode);
+    console.log(JSON.parse(body));
+  });
+}
+
+// readOneAuthor(4000);
+
+//  Update     - PUT/PATCH
+const updateAuthor = (id, name, last_name, nacionalidad, biography, gender, age) => {
+  const URL = `${URL_BASE}/api/v1/authors/${id}/`
+  const jsonSend = {
+    "name": name,
+    "last_name": last_name,
+    "nacionalidad": nacionalidad,
+    "biography": biography,
+    "gender": gender,
+    "age": age
+  };
+  request.put(URL, { form: jsonSend }, (err, res, body) => {
+    console.log(res.statusCode);
+    console.log(JSON.parse(body));
+  });
+}
+
+// updateAuthor(3333, "Yo merengues", "Pacheco", "USA", "Aclamado mexicano que nacio en USA", "M", 10);
+
+//  Delete     - DELETE
+const deleteAuthor = (id) => {
+  const URL = `${URL_BASE}/api/v1/authors/${id}/`;
+  request.delete(URL, (err, res, body) => {
+    console.log(res.statusCode);
+    // console.log(JSON.parse(body)); No tiene sentido parsear una respuesta vacía...
+    if (res.statusCode === 204) console.log('Eliminado correctamente!');
+  });
+}
+
+// deleteAuthor(4000);
