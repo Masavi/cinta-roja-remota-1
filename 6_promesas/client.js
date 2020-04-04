d55ab2242ccbd14dd4bf190fe1278dc800b7505b
maui <maui@devf.mx>
2020-04-03 19:24:46 -0600

    Agregar CREATE con promesa

--- 6_promesas/client.js
+++ 6_promesas/client.js
@@ -31,17 +31,50 @@
  *  Ejemplo de Promesa con Bloque de arriba (numero aleatorio)
  */
 
-const promise = new Promise((resolve, reject) => {
-  const number = Math.floor(Math.random() * 10);
+// const promise = new Promise((resolve, reject) => {
+//   const number = Math.floor(Math.random() * 10);
 
-  setTimeout(() => number > 5 
-      ? resolve(number) 
-      : reject(new Error('El numero es menor a 5'))
-  , 1500)
-});
+//   setTimeout(() => number > 5 
+//       ? resolve(number) 
+//       : reject(new Error('El numero es menor a 5'))
+//   , 1500)
+// });
 
-console.log(promise);
+// console.log(promise);
  
-promise
-  .then(number => console.log(number))
-  .catch(error => console.error(error));
+// promise
+//   .then(number => console.log(number))
+//   .catch(error => console.error(error));
+
+/**
+ *  CRUD de Peticiones empleando PROMESAS
+ */
+const request = require('request');
+
+const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com';
+
+//  Create - POST
+const createPromise = (name, last_name, nacionalidad, biography, gender, age) => {
+  const URL = `${URL_BASE}/api/v1/authors/`
+  const jsonSend = {
+    "name": name,
+    "last_name": last_name,
+    "nacionalidad": nacionalidad,
+    "biography": biography,
+    "gender": gender,
+    "age": age
+  };
+
+  return new Promise((resolve, reject) => {
+    request.post(URL, { form: jsonSend }, (err, res, body) => {
+      res.statusCode === 201
+        ? resolve(JSON.parse(body))
+        : reject({ message: 'Error creating author', body: body })
+    });
+  })
+}
+
+createPromise("Pedro", "Páramo", "ABC", "Anda buscando a su papá en Comala", "M", 34)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
